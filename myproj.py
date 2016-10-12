3081b1925dd823ab76dc46db6526ce6da599bdd0
Regis <boudinot.regis@yahoo.com>
2016-10-12 17:39:06 -0500

    hammy ham ham

---
+++ myproj.py
@@ -0,0 +1,16 @@
+import requests
+
+headers = {
+    'Content-type': 'application/json',
+    'Accept': 'text/plain',
+    'dial': '100'
+}
+
+res = requests.post('http://10.0.0.230:5000', headers=headers)
+
+print 'REQUEST SENT'
+
+if res.ok:
+    print res
+else:
+    print 'UHH OH'
