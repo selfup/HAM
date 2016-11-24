ca9c385e57e03ade75c22991d3aac8cc7993d32b
Regis <boudinot.regis@yahoo.com>
2016-11-24 14:49:38 -0600

    format

--- ruby/tcp_stram_read.rb
+++ ruby/tcp_stram_read.rb
@@ -3,10 +3,11 @@ require 'pp'
 
 s = TCPSocket.new '10.0.0.18', 4992
 
-format_it = -> (msg) {
-  msg[0].split("\n")
-  .map { |e| e.split(' ') }
-  .map { |e| key = e[0]; e.shift; {key => e}; }
+format_it = -> msg {
+  msg[0]
+    .split("\n")
+    .map { |e| e.split(' ') }
+    .map { |e| key = e[0]; e.shift; {key => e} }
 }
 
 loop do
