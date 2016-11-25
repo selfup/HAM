6b0f7cd8fed54babeb61d0747c51d39d375206c6
Regis <boudinot.regis@yahoo.com>
2016-11-25 17:33:52 -0600

    architecture check

--- src/main.rb
+++ src/main.rb
@@ -1,17 +1,16 @@
+return puts "RPI ARMV7 ONLY"; exit if !RUBY_PLATFORM.include?("armv7l-linux")
+
 require 'socket'
 require 'pi_piper'
 require 'pry'
 
 ## state
-
 require_relative './state'
 
 ## functions
-
 require_relative './fns'
 
 ## main
-
 @socket = TCPSocket.new('10.0.0.18', 4992)
 @socket.puts('c1|sub slice all')
 
