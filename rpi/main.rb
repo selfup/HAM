cda43e4b7fba0c2dd9052b91f6eacbfa439605ee
Regis <boudinot.regis@yahoo.com>
2016-11-25 08:54:31 -0600

    comments to server file - pry to Gemfile mostly for rpi

--- rpi/main.rb
+++ rpi/main.rb
@@ -1,3 +1,5 @@
+# this is a sinatra web server that lives on a raspberry pi
+# it will receive a post request and then use pi_piper to turn on or off GPIO
 require 'sinatra'
 require 'json'
 require 'pi_piper'
@@ -5,6 +7,7 @@ require 'pi_piper'
 # using raspberry pi GPIO bottom row pins (left to right)
 # GPIO pin numbers -> 2, 3, 4, 17, 27, 22, 10, 9
 
+# translates payload keys to selected GPIO pins
 payload_to_pin_key = {
   "1" => 2,
   "2" => 3,
@@ -16,7 +19,10 @@ payload_to_pin_key = {
   "8" => 9
 }
 
+# if respective key:value is set to true -> turn on GPIO pin
+# if respective key:value is set to false -> turn off GPIO pin
 pin_logic_gate = -> pins {
+  puts pins # just for simple debugging
   pins.each { |k, v|
     pin = PiPiper::Pin.new(
       :pin => payload_to_pin_key[k],
@@ -27,6 +33,9 @@ pin_logic_gate = -> pins {
   }
 }
 
+# handle host post request
+# grab payload and translate using payload_to_pin_key
+# once keys are translated -> send to parse gate to turn on or off GPIO pins
 post '/' do
   payload = JSON.parse(request.body.read)
   pins = payload["pins"]
