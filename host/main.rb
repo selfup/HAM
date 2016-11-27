ab636da3c3bd9c52ee1aecce6149b25eef3ca280
Regis <boudinot.regis@yahoo.com>
2016-11-27 08:41:46 -0600

    flex back up

--- host/main.rb
+++ host/main.rb
@@ -6,13 +6,12 @@ require_relative './state'
 require_relative './fns'
 ## main
 if __FILE__ == $0
-  # @flex_socket = TCPSocket.new('10.0.0.18', 4992)
-  # @flex_socket.puts('c1|sub slice all')
+  @flex_socket = TCPSocket.new('10.0.0.18', 4992)
+  @flex_socket.puts('c1|sub slice all')
   loop do
     pi_socket = TCPSocket.new('10.0.0.230', 2000)
-    # @read_and_update.(@flex_socket.recv(1000), pi_socket)
+    @read_and_update.(@flex_socket.recv(1000), pi_socket)
     @pi_logic.(pi_socket)
-    sleep(1)
     puts "--------------------------------------"
     puts Time.now.utc
     puts @app_pins
