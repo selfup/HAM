e4e2fd754513d1fd8b10fbcb7934f0f6ddc6f9c0
Regis <boudinot.regis@yahoo.com>
2016-11-27 08:23:05 -0600

    ok

--- rpi/main.rb
+++ rpi/main.rb
@@ -13,10 +13,10 @@ if __FILE__ == $0
     Thread.new(socket_server.accept) do |client|
       loop do
         @print_or_close.(client.recvmsg[0], client)
-        puts "|||||||||||||||||||||||||||||||||||||"
+        puts "==="
         p Time.now.utc
         p @app_pins
-        puts "|||||||||||||||||||||||||||||||||||||"
+        puts "==="
         GC.start
       end
     end
